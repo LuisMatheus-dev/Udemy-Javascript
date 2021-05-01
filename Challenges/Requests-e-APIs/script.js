28d1fb138130920fec9b262c574f7ad7892e86eb
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2021-05-01 08:50:41 -0300

    :hammer: [refactor] Implementado Async/Await

--- Challenges/Requests-e-APIs/script.js
+++ Challenges/Requests-e-APIs/script.js
@@ -26,39 +26,71 @@ const renderError = message => {
     countriesContainer.insertAdjacentText('beforeend',`Error: ${message}`)
 }
 
-const getJSON = function(url, errorMessage="Ops! Something went wrong") {
+
+//! Lendo dados atravez e callbacks
+// const getJSON = function(url, errorMessage="Ops! Something went wrong") {
     
-    return fetch(url)
-    .then(response => { 
+//     return fetch(url)
+//     .then(response => { 
         
-        if(!response.ok) {
-            throw new Error(`${errorMessage},\n ${response.status}`)
-        } else {
-            return response.json()
-        }
-    })
+//         if(!response.ok) {
+//             throw new Error(`${errorMessage},\n ${response.status}`)
+//         } else {
+//             return response.json()
+//         }
+//     })
+// }
+
+// const getCountriesAndNeighbour = function(country) {
+
+//     getJSON(`https://restcountries.eu/rest/v2/name/${country}`,"❌ Country not found")
+//         .then(data => {
+//             const neighbour = data[0].borders[0]
+
+//             renderCountry(data[0])
+//             console.log(data[0])
+//             if(neighbour) {
+//                 return  getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`, "❌ Country not found");
+//             } else {
+//                 throw new Error('❌ No Neighbour found')
+//             }
+//         })
+//         .then(data => renderCountry(data, "neighbour"))
+//         .catch(error => { 
+//             renderError(error.message)
+//              console.log(error.message)
+//         })
+//         .finally(() => countriesContainer.style.opacity = 1)
+// }
+
+// getCountriesAndNeighbour('brazil')
+
+//! Lendo dados atravez de async, await
+
+const getCountriesAndNeighbour = async function(country) {
+
+    const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+    const [ data ] = await response.json();
+    
+    if(!response.ok) {
+       throw new Error('❌ Country not found')
+    
+    } else {
+        renderCountry(data)
+    }
+
+    const neighbour = data.borders[0]
+    
+    if(!neighbour) {
+        throw new Error("❌ No Neighbour found")
+    
+    } else {
+        const resNeighbour = await fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+        const dataNeigbour = await resNeighbour.json();
+        renderCountry(dataNeigbour,'neighbour');
+    }
+
+    countriesContainer.style.opacity = 1;
 }
 
-const getCountriesAndNeighbour = function(country) {
-
-    getJSON(`https://restcountries.eu/rest/v2/name/${country}`,"❌ Country not found")
-        .then(data => {
-            const neighbour = data[0].borders[0]
-
-            renderCountry(data[0])
-            console.log(data[0])
-            if(neighbour) {
-                return  getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`, "❌ Country not found");
-            } else {
-                throw new Error('❌ No Neighbour found')
-            }
-        })
-        .then(data => renderCountry(data, "neighbour"))
-        .catch(error => { 
-            renderError(error.message)
-             console.log(error.message)
-        })
-        .finally(() => countriesContainer.style.opacity = 1)
-}
-
-getCountriesAndNeighbour('brazil')
+getCountriesAndNeighbour('brazil');
